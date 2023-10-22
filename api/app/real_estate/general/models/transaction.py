7051ba720539c6ec2fc331f21b33434e1e92837e
Surgeon76 <Surgeon76@yandex.ru>
2023-10-22 09:47:01 +0300

    add table term_transaction

---
+++ api/app/real_estate/general/models/transaction.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from sqlalchemy import (
+    Boolean,
+    Integer
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship
+)
+
+from config.database import Base
+
+
+class TermTransaction(Base):
+    __tablename__ = 'term_transaction'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    ipoteka: Mapped[bool] = mapped_column(Boolean, default=False)
+    part: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    rooms: Mapped['Rooms'] = relationship(back_populates='transaction',
+                                          uselist=False)
+    apartment: Mapped['Apartment'] = relationship(back_populates='transaction',
+                                                  uselist=False)
+    construction: Mapped['Construction'
+                         ] = relationship(back_populates='transaction',
+                                          uselist=False)
+
+    def __repr__(self) -> str:
+        return f"{self.__dict__}"
+
+
+"""
+    ipoteka  'Можно в ипотеку']
+    part     'Продажа доли']
+"""
