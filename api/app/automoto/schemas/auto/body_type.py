4211b4cfbab9e82b707e830fddfed3af63ff7214
Surgeon-76 <surgeon76@yandex.ru>
2023-10-30 11:02:05 +0300

    add schemas body_type

--- api/app/automoto/schemas/auto/body_type.py
+++ api/app/automoto/schemas/auto/body_type.py
@@ -1,34 +1,76 @@
-from __future__ import annotations
-
-from sqlalchemy import (
-    Integer,
-    String,
-    ForeignKey
-)
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    relationship
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field
 )
 
-from config.database import Base
+
+class BaseBodyType(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    model_id: int = Field(description='--> ID модели авто')
+    type_body: str = Field(description='Тип кузова',
+                           default='Седан')
 
 
-class BodyType(Base):
-    __tablename__ = 'body_type'
+class BodyTypeID(BaseModel):
+    id: int = Field(description='ID типа кузова')
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    model_id: Mapped[int] = mapped_column(Integer, ForeignKey('model_auto.id'))
-    type_body: Mapped[str] = mapped_column(String(), default='Седан')
 
+class BodyTypeView(BaseBodyType, BodyTypeID):
+    pass
+
+
+class BodyTypeCreate(BaseBodyType):
+    pass
+
+
+class BodyTypeUpdate(BaseBodyType, BodyTypeID):
+    pass
+
+# #############################################################
+
+
+class BodyTypeResp(BaseModel):
+    status: str = 'succes'
+    status_code: int
+    payload: BodyTypeView | None = None
+
+
+class BodyTypeError(BaseModel):
+    status: str = 'failure'
+    status_code: int
+    payload: dict | None = None
+
+
+class BodyTypePage(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    status: str = 'succes'
+    status_code: int = 200
+    payload: list[BodyTypeView] | None = None
+    total: int
+    page: int
+    size: int
+    pages: int
+
+
+class BodyTypePageError(BaseModel):
+    status: str = 'failure'
+    status_code: int
+    payload: list | None = None
+
+
+class BodyTypeDel(BaseModel):
+    status: str = 'succes'
+    status_code: int
+    payload: dict | None = None
+
+
+"""
     avtotth: Mapped['AvtoTth'] = relationship(back_populates='bodytype',
                                               uselist=False)
     model: Mapped['ModelAuto'] = relationship(back_populates='body')
 
-    def __repr__(self) -> str:
-        return f"{self.__dict__}"
-
-
-"""
     type_body 'Тип кузова: Седан, Хэтчбек, Универсал, Кабриолет, Купе'
 """
