e973ef3c9f085b0bbc32a608d5eb0ea09055dd7c
Surgeon-76 <surgeon76@yandex.ru>
2023-10-30 13:28:55 +0300

    add schemas model_auto

--- api/app/automoto/schemas/auto/model_auto.py
+++ api/app/automoto/schemas/auto/model_auto.py
@@ -1,33 +1,74 @@
-from __future__ import annotations
-
-from sqlalchemy import (
-    Integer,
-    String,
-    ForeignKey
-)
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    relationship
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field
 )
 
-from config.database import Base
+
+class BaseModelAuto(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    brand_id: int = Field(description='Модель авто')
+    model: str = Field(description='Модель авто')
 
 
-class ModelAuto(Base):
-    __tablename__ = 'model_auto'
+class ModelAutoID(BaseModel):
+    id: int = Field(description='ID модели авто')
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    brand_id: Mapped[int] = mapped_column(Integer, ForeignKey('brand_auto.id'))
-    model: Mapped[str] = mapped_column(String())
 
-    body: Mapped[list['BodyType']] = relationship(back_populates='model')
-    brand: Mapped['BrandAuto'] = relationship(back_populates='model')
+class ModelAutoView(BaseModelAuto, ModelAutoID):
+    pass
 
-    def __repr__(self) -> str:
-        return f"{self.__dict__}"
+
+class ModelAutoCreate(BaseModelAuto):
+    pass
+
+
+class ModelAutoUpdate(BaseModelAuto, ModelAutoID):
+    pass
+
+# #############################################################
+
+
+class ModelAutoResp(BaseModel):
+    status: str = 'succes'
+    status_code: int
+    payload: ModelAutoView | None = None
+
+
+class ModelAutoError(BaseModel):
+    status: str = 'failure'
+    status_code: int
+    payload: dict | None = None
+
+
+class ModelAutoPage(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    status: str = 'succes'
+    status_code: int = 200
+    payload: list[ModelAutoView] | None = None
+    total: int
+    page: int
+    size: int
+    pages: int
+
+
+class ModelAutoPageError(BaseModel):
+    status: str = 'failure'
+    status_code: int
+    payload: list | None = None
+
+
+class ModelAutoDel(BaseModel):
+    status: str = 'succes'
+    status_code: int
+    payload: dict | None = None
 
 
 """
+    body: Mapped[list['BodyType']] = relationship(back_populates='model')
+    brand: Mapped['BrandAuto'] = relationship(back_populates='model')
+
     model 'Модель авто'
 """
