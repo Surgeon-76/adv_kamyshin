f89f8c1f4963a7baf2fe60b75f874d4451cfd317
Surgeon-76 <surgeon76@yandex.ru>
2023-10-31 12:32:27 +0300

    add cshemas moto_type

--- api/app/automoto/schemas/moto/moto_type.py
+++ api/app/automoto/schemas/moto/moto_type.py
@@ -1,34 +1,76 @@
-from __future__ import annotations
-
-from sqlalchemy import (
-    Integer,
-    String,
-    ForeignKey
-)
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    relationship
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field
 )
 
-from config.database import Base
+
+class BaseMotoType(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    model_id: int = Field(description='--> model_moto.id')
+    type_moto: str = Field(description='Тип',
+                           default='Туристический')
 
 
-class MotoType(Base):
-    __tablename__ = 'moto_type'
+class MotoTypeID(BaseModel):
+    id: int = Field(description='ID бренда мото')
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    model_id: Mapped[int] = mapped_column(Integer, ForeignKey('model_moto.id'))
-    type_moto: Mapped[str] = mapped_column(String(), default='Туристический')
 
+class MotoTypeView(BaseMotoType, MotoTypeID):
+    pass
+
+
+class MotoTypeCreate(BaseMotoType):
+    pass
+
+
+class MotoTypeUpdate(BaseMotoType, MotoTypeID):
+    pass
+
+# #############################################################
+
+
+class MotoTypeResp(BaseModel):
+    status: str = 'succes'
+    status_code: int
+    payload: MotoTypeView | None = None
+
+
+class MotoTypeError(BaseModel):
+    status: str = 'failure'
+    status_code: int
+    payload: dict | None = None
+
+
+class MotoTypePage(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    status: str = 'succes'
+    status_code: int = 200
+    payload: list[MotoTypeView] | None = None
+    total: int
+    page: int
+    size: int
+    pages: int
+
+
+class MotoTypePageError(BaseModel):
+    status: str = 'failure'
+    status_code: int
+    payload: list | None = None
+
+
+class MotoTypeDel(BaseModel):
+    status: str = 'succes'
+    status_code: int
+    payload: dict | None = None
+
+
+"""
     mototth: Mapped['MotoTth'] = relationship(back_populates='mototype',
                                               uselist=False)
     model: Mapped['ModelMoto'] = relationship(back_populates='mototype')
 
-    def __repr__(self) -> str:
-        return f"{self.__dict__}"
-
-
-"""
     type_moto 'Спортивный, Туристический, Чоппер и т.д.'
 """
